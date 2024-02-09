3f52cae2d8f15559dcc750e116744d5b9487ee20
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-02-09 03:01:21 +0200

    Add files via upload

---
+++ opkg install gstreamer1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo
+opkg install enigma2-plugin-systemplugins-serviceapp
+opkg install --force-reinstall enigma2-plugin-systemplugins-serviceapp
+opkg install gstreamer1.0-plugins-good
+opkg install gstreamer1.0-plugins-base
+opkg install gstreamer1.0-plugins-bad
+opkg install gstreamer1.0-plugins-ugly
+opkg install gstreamer1.0-plugins-bad-meta
+opkg install enigma2-plugin-systemplugins-softcamstartup 
+sleep 2;
+exit
