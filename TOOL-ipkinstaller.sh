7fb0a76880d248156f7ced6ff9ee967cdea727c0
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-05-15 17:10:23 +0300

    Update TOOL-ipkinstaller.sh

--- TOOL-ipkinstaller.sh
+++ TOOL-ipkinstaller.sh
@@ -1,4 +1,17 @@
-opkg install --force-overwrite /tmp/*.ipk
+#!/bin/sh
+#
+
+wget -O /tmp/ipkuninstaller.tar.gz "https://gitlab.com/hanfy1971/plugin/-/raw/main/ipkuninstaller/ipkuninstaller.tar.gz"
+
+tar -xzf /tmp/ipkuninstaller.tar.gz -C /
+
 wait
-sleep 2;
+
+rm -f /tmp/ipkuninstaller.tar.gz
+echo "   UPLOADED BY  >>>>   TAREK_TT "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+"**********************************************************************************"
+wait
+killall -9 enigma2
 exit 0
